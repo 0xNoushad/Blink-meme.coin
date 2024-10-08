3ba4a5e7b62f67206661368b7b396ba2580de219
0xNoushad <choudharynoushad46@gmail.com>
2024-10-08 23:37:45 +0530

    changes

--- app/actions.json/route.ts
+++ app/actions.json/route.ts
@@ -3,21 +3,19 @@ import { ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
 export const GET = async () => {
   const payload: ActionsJson = {
     rules: [
+      // map all root level routes to an action
       {
-        pathPattern: "/*",
-        apiPath: "/api/actions/meme",
-      },
-      {
-        pathPattern: "/api/actions/meme",
+        pathPattern: "/",
         apiPath: "/api/actions/meme",
       },
     ],
   };
 
-  return new Response(JSON.stringify(payload), {
+  return Response.json(payload, {
     headers: ACTIONS_CORS_HEADERS,
   });
 };
 
-// Include OPTIONS method to ensure CORS works for Blinks
+// DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
+// THIS WILL ENSURE CORS WORKS FOR BLINKS
 export const OPTIONS = GET;
